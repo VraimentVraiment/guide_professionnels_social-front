98da2625e9ea923af754c131f5a14abe49d31d0b
ShallowRed <48523123+ShallowRed@users.noreply.github.com>
2023-07-26 12:21:05 +0200

    debuging middleware

--- client/composables/auth/useIsAuthenticated.ts
+++ client/composables/auth/useIsAuthenticated.ts
@@ -5,8 +5,10 @@ export function useIsAuthenticated (): ComputedRef<boolean> {
   const user = useDirectusUser()
 
   return computed(() => {
+    console.log('user.value :', user.value)
     return Boolean(user.value)
     // return false
     // return true
   })
 }
+#
